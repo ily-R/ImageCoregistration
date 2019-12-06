fe2779ba0f659d381bc7c021b8f3860a480d3809
ilyasAr <arouiilyes@gmail.com>
2019-12-06 02:35:20 +0100

    added comments and ransac

--- image_register.py
+++ image_register.py
@@ -11,6 +11,16 @@ from utils import *
 
 
 def readAndRescale(img1, img2, scale):
+    """Helper to read images, scale them and convert to grayscale.
+        it returns original, gray and scaled images
+
+    Typical use:
+        t, s, t_gray, s_gray, t_full, s_full = readAndRescale("cat1.jpg", "cat2.jpg", 0.3)
+
+    img1: target image name
+    img2: source image name
+    scale: scaling factor, keeping aspect ratio
+    """
     target = cv2.imread(os.path.join("data", img1))
     source = cv2.imread(os.path.join("data", img2))
 
@@ -27,7 +37,16 @@ def readAndRescale(img1, img2, scale):
     return target_s, source_s, gray1, gray2, target, source
 
 
-def getClicksAndDiscriptor(target, source, target_gray, source_gray):
+def getClicksAndDescriptor(target, source, target_gray, source_gray):
+    """Helper to get user mouse-clicks, use them as landmarks for sift descriptors.
+        it returns the closets real sift to these landmarks and their descriptors
+
+    Typical use:
+        lmk1, lmk2, desc1, desc2 = getClicksAndDescriptor(target, source, target_gray, source_gray)
+
+    target, source: target and source images as np.ndarray
+    target_gray, source_gray: grayscaled target and source images as np.ndarray
+    """
     def getClick(event, x, y, flags, param):
         global p1, p2, on_target
         if event == cv2.EVENT_LBUTTONDOWN:
@@ -64,10 +83,24 @@ def getClicksAndDiscriptor(target, source, target_gray, source_gray):
     pts1_all = np.array([kp1[idx].pt for idx in range(0, len(kp1))])
     kp2, des2 = sift.detectAndCompute(source_gray, None)
     pts2_all = np.array([kp2[idx].pt for idx in range(0, len(kp2))])
-    return pts1_all[:p1.shape[0]], pts2_all[:p1.shape[0]], des1[:p1.shape[0]], des2[:p1.shape[0]]
+    indices1, indices2 = [], []
+    for i in range(p1.shape[0]):
+        distance = np.sqrt(np.sum((p1[i] - pts1_all) ** 2, axis=1))
+        indices1.append(np.argmin(distance))
+        distance = np.sqrt(np.sum((p2[i] - pts2_all) ** 2, axis=1))
+        indices2.append(np.argmin(distance))
+    return pts1_all[indices1], pts2_all[indices2], des1[indices1], des2[indices2]
 
 
-def getKeypointAndDiscriptors(target_gray, source_gray):
+def getKeypointAndDescriptors(target_gray, source_gray):
+    """Helper to get Harris points of interest and use them as landmarks for sift descriptors.
+        it returns these landmarks and their descriptors
+
+    Typical use:
+        lmk1, lmk2, desc1, desc2 = getKeypointAndDescriptors(target_gray, source_gray)
+
+    target_gray, source_gray: grayscaled target and source images as np.ndarray
+    """
     sift = cv2.xfeatures2d.SIFT_create()
     kp1, des1 = sift.detectAndCompute(target_gray, None)
     pts1 = np.array([kp1[idx].pt for idx in range(len(kp1))])
@@ -84,7 +117,7 @@ if __name__ == "__main__":
                         help='rescale the images with this factor in range [0, 1]')
     parser.add_argument("--sift", help="apply harris point of interest detect and sift descriptor", action="store_true")
 
-    parser.add_argument("--ransac", help="apply ransac", action="store_true")
+    parser.add_argument("-r","--ransac", help="apply ransac", action="store_true")
 
     args = parser.parse_args()
     target, source, target_gray, source_gray, target_full, source_full = readAndRescale(args.target, args.source, args.scale)
@@ -92,14 +125,18 @@ if __name__ == "__main__":
     if not args.sift:
         on_target = True
         p1, p2 = [], []
-        lmk1, lmk2, desc1, desc2 = getClicksAndDiscriptor(target, source, target_gray, source_gray)
+        lmk1, lmk2, desc1, desc2 = getClicksAndDescriptor(target, source, target_gray, source_gray)
     else:
-        lmk1, lmk2, desc1, desc2 = getKeypointAndDiscriptors(target_gray, source_gray)
+        lmk1, lmk2, desc1, desc2 = getKeypointAndDescriptors(target_gray, source_gray)
 
     lmk1, lmk2 = match(lmk1, lmk2, desc1, desc2)
-    display_matches(target, source, lmk1, lmk2)
+    display_matches(target, source, lmk1, lmk2, name="matches")
     if args.ransac:
-        lmk1, lmk2 = ransac(lmk1, lmk2)
+        lmk1, lmk2, outliers1, outliers2 = ransac(lmk1, lmk2)
+        display_matches(target, source, outliers1, outliers2, name="matches_removed_by_RANSAC", num=5, save=True)
 
     T = calculate_transform(lmk2, lmk1)
-    w = warp(target, source, T)
+    warped, target_w = warp(target, source, T)
+    cc = cross_corr(warped, target_w)
+    mi = mutual_inf(warped, target_w, verbose=True)
+
--- utils.py
+++ utils.py
@@ -6,9 +6,23 @@ Created on Thu Dec 06 10:24:14 2019
 
 import cv2
 import numpy as np
+import os
+from sklearn import linear_model
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
-def display_matches(img1, img2, kp1, kp2, num=20, save=None):
+def display_matches(img1, img2, kp1, kp2,name, num=20, save=False):
+    """Helper to display matches of keypoint in botch images, by connecting a line from one image to another
+
+    Typical use:
+        display_matches(target, source, lmk1, lmk2, name="matches", save = True)
+
+    img1, img2: target and source images as np.ndarray
+    kp1, kp2: landmarks of target and source images respectively as np.ndarray
+    name: name of the figure display and the image saved if save = True
+    save: boolean indicates to save the image of the matches
+    """
     if img1.shape[0] != img2.shape[0]:
         minn = min(img1.shape[0], img1.shape[0])
         if minn == img1.shape[0]:
@@ -16,20 +30,31 @@ def display_matches(img1, img2, kp1, kp2, num=20, save=None):
         else:
             img2 = np.concatenate((img2, np.zeros(img1.shape[0] - minn, img2.shape[1], 3)), axis=0)
     img = np.concatenate((img1, img2), axis=1)
-    for i in np.random.choice(len(kp1), num):
+    for i in np.random.choice(len(kp1), min(num, len(kp1))):
         x1, y1 = int(kp1[i][0]), int(kp1[i][1])
         x2, y2 = int(kp2[i][0]) + img1.shape[1], int(kp2[i][1])
         cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 1)
-    cv2.namedWindow('jpg', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('jpg', img.shape[1], img.shape[0])
-    cv2.imshow('jpg', img)
+    cv2.namedWindow(name, cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(name, img.shape[1], img.shape[0])
+    cv2.imshow(name, img)
     cv2.waitKey()
     cv2.destroyAllWindows()
-    if save is not None:
-        cv2.imwrite(save, img)
+    if save:
+        cv2.imwrite(os.path.join("result", name+".jpg"), img)
 
 
 def match(lmk1, lmk2, desc1, desc2, sift_error=0.7):
+    """Helper to find the pair of matches between two keypoints lists
+    it return two np.ndarray of landmarks in an order respecting the matching
+
+    Typical use:
+        lmk1, lmk2 = match(lmk1, lmk2, desc1, desc2)
+
+    lmk1, lmk2: landmarks of target and source images respectively as np.ndarray
+    desc1, desc2: descriptors of target and source images respectively as np.ndarray
+    sift_error: if the ratio between the distance to the closest match and the second closest is less than sift_error
+    reject this landmark.
+    """
     match1, match2 = [], []
     for i in range(len(desc1)):
         distance = np.sqrt(np.sum((desc1[i] - desc2) ** 2, axis=1))
@@ -41,18 +66,88 @@ def match(lmk1, lmk2, desc1, desc2, sift_error=0.7):
 
 
 def cross_corr(img1, img2):
-    pass
+    """Helper to calculate cross_correlation metric between two images. Well adapted, if we assume there is a linear
+    transformation between pixels intensities in both images.
+    it returns the cross-correlation value.
+
+    Typical use:
+        cc = cross_corr(warped, target_w)
+    """
+    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    mean1, mean2 = np.mean(img1), np.mean(img2)
+    img1, img2 = img1-mean1, img2-mean2
+    numerator = np.sum(np.multiply(img1, img2))
+    denominator = np.sqrt(np.sum(np.multiply(img1, img1))*np.sum(np.multiply(img2, img2)))
+    corr = numerator/denominator
+    print("Cross-correlation: ", corr)
+    return corr
 
 
-def mutual_inf(img1, img2):
-    pass
+def mutual_inf(img1, img2, verbose=False):
+    """Helper to calculate mutual-information metric between two images. it gives a probabilistic measure on how
+    uncertain we are about the target image in the absence/presence of the warped source image
+    it returns the mutual information value.
+
+    Typical use:
+        mi = mutual_inf(warped, target_w)
+
+    verbose: if verbose=True, display and save the joint-histogram between the two images.
+    """
+    epsilon = 1.e-6
+    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    img1 = np.round(img1).astype("uint8")
+    img2 = np.round(img2).astype("uint8")
+
+    joint_hist = np.zeros((256, 256))
+    for i in range(min(img1.shape[0], img2.shape[0])):
+        for j in range(min(img1.shape[1], img2.shape[1])):
+            joint_hist[img1[i, j], img2[i, j]] += 1
+
+    if verbose:
+        display_jh = np.log(joint_hist + epsilon)
+        display_jh = 255*(display_jh - display_jh.min())/(display_jh.max() - display_jh.min())
+        cv2.imshow("joint_histogram", display_jh)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+        cv2.imwrite("result/joint_histogram.jpg", display_jh)
+
+    joint_hist /= np.sum(joint_hist)
+    p1 = np.sum(joint_hist, axis=0)
+    p2 = np.sum(joint_hist, axis=1)
+    joint_hist_d = joint_hist/(p1+epsilon)
+    joint_hist_d /= (p2+epsilon)
+    mi = np.sum(np.multiply(joint_hist, np.log(joint_hist_d+epsilon)))
+    print("Mutual Information: ", mi)
+    return mi
 
 
 def ransac(kp1, kp2):
-    pass
+    """Helper to apply ransac (RANdom SAmple Consensus) algorithm on two arrays of landmarks
+    it returns the inliers and outliers in both arrays
+
+    Typical use:
+        lmk1, lmk2, outliers1, outliers2 = ransac(lmk1, lmk2)
+
+    kp1, kp2: landmarks of target and source images respectively as np.ndarray
+    """
+    ransac_model = linear_model.RANSACRegressor()
+    ransac_model.fit(kp1, kp2)
+    inlier_mask = ransac_model.inlier_mask_
+    outlier_mask = np.logical_not(inlier_mask)
+    return kp1[inlier_mask], kp2[inlier_mask], kp1[outlier_mask], kp2[outlier_mask]
 
 
 def calculate_transform(kp1, kp2):
+    """Helper to apply find the best affine transform using two arrays of landmarks.
+    it returns the affine transform, a matrix T of size (2, 3)
+
+    Typical use:
+        T = calculate_transform(lmk2, lmk1)
+
+    kp1, kp2: landmarks of target and source images respectively as np.ndarray
+    """
     upper = np.concatenate((kp1, np.ones((kp1.shape[0], 1)), np.zeros((kp1.shape[0], 3))), axis=1)
     lower = np.concatenate((np.zeros((kp1.shape[0], 3)), kp1, np.ones((kp1.shape[0], 1))), axis=1)
     X = np.concatenate((upper, lower), axis=0)
@@ -64,11 +159,21 @@ def calculate_transform(kp1, kp2):
     kp2_pred = np.dot(T, np.concatenate((kp1, np.ones((kp1.shape[0], 1))), axis=1).T).T
     kp2_pred /= kp2_pred[:, -1:]
     error = np.linalg.norm(kp2_pred[:, :2] - kp2)
-    print("reconstruction error: ", error)
+    print("coordinate reconstruction error: ", error)
     return T
 
 
 def warp(target, source, T):
+
+    """Helper to move the source image to the same reference as target image, so they can be co-registered.
+    it returns the new warped source image and the target image which is also centered in a larger figure by 10 pixels.
+    i.e, if the input size is (M, N) then the output is (M+10, N+10).
+
+    Typical use:
+        warped, target_w = warp(target, source, T)
+
+    T:  affine transform, a matrix T of size (2, 3)
+    """
     height = target.shape[0]
     width = source.shape[1]
 
@@ -78,11 +183,11 @@ def warp(target, source, T):
     T_perspective = cv2.getPerspectiveTransform(corners, corners_moved)
     target_new = cv2.warpPerspective(target, T_perspective, (width + 10, height + 10))
     cv2.imshow("target_new", target_new)
-    cv2.imwrite("target_new.jpg", target_new)
+    cv2.imwrite("result/target_new.jpg", target_new)
     T = np.dot(T_perspective, T)
     source_new = cv2.warpPerspective(source, T, (width + 10, height + 10), cv2.INTER_AREA)
     cv2.imshow("source_new", source_new)  # show transform
-    cv2.imwrite("source_new.jpg", source_new)
+    cv2.imwrite("result/source_new.jpg", source_new)
     cv2.waitKey()
     cv2.destroyAllWindows()
-    return source_new
+    return source_new, target_new
